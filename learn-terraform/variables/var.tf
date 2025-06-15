6279c39788ac801a6708885d5ecbb70063b96fb5
JyotiPanigrahi <panigrahij95@gmail.com>
2025-06-15 17:27:27 +0530

    created var file

---
+++ learn-terraform/variables/var.tf
@@ -0,0 +1,32 @@
+variable "x"{
+    default = 100
+}
+
+variable "y"{
+    default = ["abc", "xyz", 9, 11]
+}
+
+variable "z"{
+    default  = {
+        x = 10
+        y = 20
+    }
+}
+
+output "x" {
+    value = var.x
+}
+
+
+output "y" {
+    value = var.y
+    
+}
+
+output "y_1" {
+    value = var.y[1]
+}
+
+output "ztoxvaluemapping" {
+    value = var.z[x]
+}
