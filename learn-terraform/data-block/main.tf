d7a0f6292c8773cacc6a699002eccf6836d980c0
JyotiPanigrahi <panigrahij95@gmail.com>
2025-06-16 20:20:40 +0530

    up

--- learn-terraform/data-block/main.tf
+++ learn-terraform/data-block/main.tf
@@ -6,4 +6,19 @@ output "sg" {
     value = data.aws_security_group.selected
 }
 
+output "instance_public_ip" {
+  value = data.aws_instance.by_id.public_ip
+}
+
+output "instance_private_ip" {
+  value = data.aws_instance.by_id.private_ip
+}
+
+output "instance_ami" {
+  value = data.aws_instance.by_id.ami
+}
+
+output "instance_tags" {
+  value = data.aws_instance.by_id.tags
+}
 
