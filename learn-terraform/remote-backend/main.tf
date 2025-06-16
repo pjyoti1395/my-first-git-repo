021dc2af99413d146638f22cfbc0c8012147b3af
JyotiPanigrahi <panigrahij95@gmail.com>
2025-06-16 21:17:19 +0530

    up

--- learn-terraform/remote-backend/main.tf
+++ learn-terraform/remote-backend/main.tf
@@ -5,23 +5,27 @@ terraform {
       version = "5.96.0"
     }
   }
+
+
+    backend "s3"{ 
+        bucket = "Jyoti-buket-1606"
+        key = "jyoti/terraform/terraform.tfstate"
+        region = "us-east-1"
+        use_lockfile = true 
+
+    }
 }
-
-backend "s3"{
-    bucket = "Jyoti-buket-1606"
-    key = "jyoti/terraform/terraform.tfstate"
-    region = "us-east-1"
-    use_lockfile = true 
-
-}
-
 
 provider "aws" {
   region = "us-east-1"
 }
 
 
-resource "aws_s3_bucket" "test" {
-  ami = "ami-0533f2ba8a1995cf9"
+resource "aws_instance" "example" {
+  ami           = "ami-0533f2ba8a1995cf9"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jyoti-EC2"
+  }
 }
