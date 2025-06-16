8b60dc21c6518e1cb5c47e65e1b40cdd32050a0b
JyotiPanigrahi <panigrahij95@gmail.com>
2025-06-16 19:17:09 +0530

    up

--- learn-terraform/data-block/ec2.tf
+++ learn-terraform/data-block/ec2.tf
@@ -1,13 +1,13 @@
 data "aws_instance" "foo" {
-  instance_id = "i-instanceid"
+  instance_id = "i-0b685d5fc3a6a2926"
 
   filter {
     name   = "image-id"
-    values = ["ami-xxxxxxxx"]
+    values = "ami-03ab4f7c25eca90dc"
   }
 
   filter {
-    name   = "tag:Name"
-    values = ["instance-name-tag"]
+    name = "tag:Name"
+    values = "web"
   }
 }
