8894fb3fb37fb9025efebdad495a0dd2d84d3080
JyotiPanigrahi <panigrahij95@gmail.com>
2025-06-16 22:26:34 +0530

    up

--- learn-terraform/remote-backend/main.tf
+++ learn-terraform/remote-backend/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 
     backend "s3"{ 
-        bucket = "my-terraform-bucket-jyoti-1995 "
+        bucket = "my-terraform-bucket-jyoti-1995"
         key = "jyoti/terraform/terraform.tfstate"
         region = "us-east-1"
         use_lockfile = true 
