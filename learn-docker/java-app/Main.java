cb8b387af5883808eb543e56c58d174bda46fd26
Jyoti Panigrahi <50869609+pjyoti1395@users.noreply.github.com>
2025-10-23 13:15:12 +0530

    Update Main.java

--- learn-docker/java-app/Main.java
+++ learn-docker/java-app/Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Main
 {
-    public static void Main(String[] args)
+    public static void main(String[] args)
     {
         Date currentDate = new Date();
         System.out.println("Hello Docker "  + currentDate);
